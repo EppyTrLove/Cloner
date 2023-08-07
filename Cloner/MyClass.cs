e06de77910e27b090d7cb0195c4c4fdca1f554c3
Nikita Epifantsev <eppy_92@mail.ru>
2023-08-07 22:10:59 +0300

    first commit

---
+++ Cloner/MyClass.cs
@@ -0,0 +1,11 @@
+﻿[Serializable]
+public class MyClass
+{
+    private int Age { get; set; }
+    private string Occupation;
+    private string Reply => $"{Age}-i-am";
+    public string FirstName { get; private set; } = "Rowan";
+    public string LastName { get; set; } = "Atkinson";
+    public MyClass() { Occupation = "Actor"; }
+    public void SetFamily(string occupation) { Occupation = occupation; }
+}
---
+++ Cloner/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+public static class Cloner
+{
+    public static T Clone<T>(this T source)
+    {
+        MemoryStream stream = new MemoryStream();
+
+        BinaryFormatter formatter = new BinaryFormatter();
+        formatter.Serialize(stream, source);
+        return (T)formatter.Deserialize(stream);
+    }
+}
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+        var i = Cloner.Clone(typeof(MyClass));
+
+        }
+    }
